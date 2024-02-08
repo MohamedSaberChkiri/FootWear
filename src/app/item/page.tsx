fb1887525fa3de1aec05e7b0d85590a4a14395e9
Saber Chkiri <75795312+MohamedSaberChkiri@users.noreply.github.com>
2024-02-08 15:06:13 +0100

    woking on upsell in product page

--- src/app/item/page.tsx
+++ src/app/item/page.tsx
@@ -1,3 +1,10 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Rating } from "@mui/material"
+import { useState } from "react"
+import Divider from '@mui/material/Divider';
+
 export default function ItemInfo(
     props :{
         productName : string,
@@ -11,19 +18,61 @@ export default function ItemInfo(
 ){
 
     const backGround = 'url("https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/b01c67f2-2481-45d7-b383-a1476d768f6e/air-force-1-07-damenschuh-dgr2tk.png")'
+    const [Con, setCon] = useState<number>(1)
+    const AddCon =()=>{
+        if(Con < 10){
+            setCon(Con +1)
+        }
+    }
+    const RmCon= ()=>{
+        if(Con > 1){
+            setCon(Con -1)
+        }else{
+            setCon(1)
+        }
+    }
     return(
         <div>
             
             <div className="flex flex-wrap items-center justify-center my-[3vh] gap-4">
-                <div className="w-[40rem] h-[40rem] border" style={{ background:  backGround, backgroundPosition: "center", backgroundSize: 'cover' }}></div>
-                <div className="w-[40rem] h-[40rem] flex flex-col items-start justify-around border">
 
-                </div>
+                <div className="w-[40rem] h-[40rem]" style={{ background:  backGround, backgroundPosition: "center", backgroundSize: 'cover' }}></div>
+
+                               {/* -------------------------------------------------------------------------------------------------------------          */}
+
+                <div className="w-[40rem] h-[40rem] flex flex-col items-start justify-around  p-[4vh]">
+                            <Rating name="read-only" value={5} readOnly/>
+                                    <p className="text-5xl font-bold">AERODAILY BASEBALL CAP</p>
+                                    <p className="font-bold text-xl">$102</p>
+                                        <div className="flex items-center justify-center gap-4">
+                                            <div className="flex items-center font-bold justify-around w-[150px] h-[5vh] border-black border">
+                                                <button onClick={RmCon} >-</button>
+                                                <div>{Con}</div>
+                                                <button onClick={AddCon}>+</button>
+                                            </div>
+                                            <Button className="w-[150px] h-[5vh] rounded-none">ADD TO CART</Button>
+                                        </div>
+
+                                        <p>here goes the long description qsfjioqsdjfisdjfiqosjf sqofjsiofj sqjf 
+                                            sfiojsqfijs fsiofjs qdfioqsjf qsdfoqs ifjiosqjdfjsdf qsof qsifjisoqdfishdfioqsj 
+                                            fjsfihsqfoijdsqfijf qsdhere goes the long description qsfjioqsdjfisdjfiqosjf sqofjsiofj sqjf 
+                                            sfiojsqfijs fsiofjs qdfioqsjf qsdfoqs ifjiosqjdfjsdf qsof qsifjisoqdfishdfioqsj 
+                                            fjsfihsqfoijdsqfijf qsdhere goes the long description qsfjioqsdjfisdjfiqosjf sqofjsiofj sqjf 
+                                            sfiojsqfijs fsiofjs qdfioqsjf qsdfoqs ifjiosqjdfjsdf qsof qsifjisoqdfishdfioqsj 
+                                            fjsfihsqfoijdsqfijf qsdhere goes the long description qsfjioqsdjfisdjfiqosjf sqofjsiofj sqjf 
+                                            sfiojsqfijs fsiofjs qdfioqsjf qsdfoqs ifjiosqjdfjsdf qsof qsifjisoqdfishdfioqsj 
+                                            fjsfihsqfoijdsqfijf qsd</p>
+                                        <h1 className="font-bold text-2xl">REVIEWS :</h1>
+                                        <Divider className="w-full"/>
+                                        <p>There are yet no reviews for this product.</p>
+                                       
+                    </div>
+                       {/* -------------------------------------------------------------------------------------------------------------          */}
             </div>
 
 
 
-            <div></div>
+           
 
 
         </div>
