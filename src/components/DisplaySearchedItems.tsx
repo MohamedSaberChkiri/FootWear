ff7786e51d9175cba5159d69d40c81cf9c0b83d9
Saber Chkiri <75795312+MohamedSaberChkiri@users.noreply.github.com>
2024-02-14 18:43:30 +0100

    v1.7

--- src/components/DisplaySearchedItems.tsx
+++ src/components/DisplaySearchedItems.tsx
@@ -1,7 +1,7 @@
 
 
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchProducts from './SearchProducts';
 
 function DisplaySearchedItems() {
@@ -11,13 +11,23 @@ function DisplaySearchedItems() {
         setSearchValue(event.target.value);
     };
 
+    useEffect(() => {
+    
+        document.body.style.overflow = 'hidden';
+    
+       
+        return () => {
+          document.body.style.overflow = 'auto';
+        };
+      }, []);
+
     return (
         <div className='absolute top-0 left-0 w-full h-full z-50 flex flex-col items-center justify-around'>
             <div className='absolute top-4 left-1/2 transform -translate-x-1/2 z-10'>
                 <input
                     type="text"
                     onChange={handleInputChange}
-                    className="w-[400px] text-white text-2xl border-b border-b-white outline-none bg-transparent pb-[20px]"
+                    className="w-[400px] text-white text-2xl border-b-2 border-b-white outline-none bg-transparent pb-[20px] mt-[5vh]"
                     placeholder="Search Products"
                 />
             </div>
