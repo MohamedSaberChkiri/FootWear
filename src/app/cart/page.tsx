9aec555e9088351dbb033533190779a6642bbba6
Saber Chkiri <75795312+MohamedSaberChkiri@users.noreply.github.com>
2024-02-18 18:36:55 +0100

    deploy

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -63,7 +63,7 @@ function Page() {
                     try {
                         
                         
-                        const response = await fetch('https://shoes-store-server-rjwmbowa8-mohamedsaberchkiris-projects.vercel.app/create-payment-session', {
+                        const response = await fetch('https://footwearserver.vercel.app/create-payment-session', {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify({cartData}),
