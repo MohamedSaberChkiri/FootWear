2569abbc0c000fa95f3d102101eaf11faa7f76e2
Saber Chkiri <mohamed.saber.chkiri@gmail.com>
2024-08-17 15:07:58 +0200

    Updating CORS

--- SERVER/server.js
+++ SERVER/server.js
@@ -14,21 +14,9 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = ["https://foot-wear-one.vercel.app"];
+const allowedOrigins = "https://foot-wear-one.vercel.app";
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg =
-        "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.use(bodyParser.json());
 
