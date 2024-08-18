0dd737adce33087ec0295154b1bd8a7215a2d816
Saber Chkiri <mohamed.saber.chkiri@gmail.com>
2024-08-18 11:16:56 +0200

    FIXED userCart Quantity patching

--- SERVER/middlewares/authMiddleware.js
+++ SERVER/middlewares/authMiddleware.js
@@ -1,12 +1,16 @@
+const jwt = require("jsonwebtoken");
+
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, SerializedUser) => {
-    if (err) return res.sendStatus(403);
-    req.user = SerializedUser;
-    next();
+  if (token == null) return res.sendStatus(401); // If no token, respond with 401 Unauthorized
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403); // If token is invalid, respond with 403 Forbidden
+
+    req.user = user; // Attach the user to the req object
+    next(); // Pass control to the next middleware or route handler
   });
 }
 
